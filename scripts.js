52dee610170bda5c5f7f185d1755f36280d758c0
tenefra <stenefra@gmail.com>
2021-01-31 23:07:16 -0600

    Init filtering push

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@
 
 let arrayOfTodos = []
 
+let filteredTodos = arrayOfTodos.filter(item => item.userId)
+
 const fetchTodos = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
@@ -18,9 +20,30 @@ const populateTodos = () => {
   })
 }
 
+const todoList = document.querySelector("#todo-list")
+
 const createElement = () => {
   for (i = 0; i < 201; i++) {
-    const todoList = document.querySelector("#todo-list")
+    const todoListItem = document.createElement("li")
+    const newItem = document.createTextNode(arrayOfTodos[i].title)
+    todoListItem.appendChild(newItem)
+    todoList.appendChild(todoListItem)
+    if (arrayOfTodos[i].completed) {
+      console.log("complete")
+      todoListItem.classList.add("complete")
+    } else {
+      console.log("incomplete")
+      todoListItem.classList.add("incomplete")
+    }
+  }
+}
+
+const filterTodos = () => {
+  let userInput = document.querySelector("#filter-input").value
+  console.log(userInput)
+  todoList.innerHTML = ""
+  let i = 0
+  if ((arrayOfTodos.userId = userInput)) {
     const todoListItem = document.createElement("li")
     const newItem = document.createTextNode(arrayOfTodos[i].title)
     todoListItem.appendChild(newItem)
