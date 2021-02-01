135741a8d9c3eeb9c5a4c135c34d7d4079071bc0
tenefra <stenefra@gmail.com>
2021-01-31 20:21:02 -0600

    fetch first project complete

--- scripts.js
+++ scripts.js
@@ -1,19 +1,6 @@
 // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
-let arrayOfTodos = [
-  {
-    userId: 14,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false
-  },
-  {
-    userId: 20,
-    id: 2,
-    title: "delectus aut autem",
-    completed: false
-  }
-]
+let arrayOfTodos = []
 
 const fetchTodos = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
@@ -27,30 +14,23 @@ const logTodos = () => {
 
 const populateTodos = () => {
   arrayOfTodos.forEach(todo => {
-    if (!todo.completed) {
-      console.log("incomplete")
-      createIncompleteElement(todo)
-    } else {
-      console.log("complete")
-      createElement(todo)
-    }
+    createElement(todo)
   })
 }
 
-const createIncompleteElement = () => {
-  const todoList = document.querySelector("#todo-list")
-  const todoListItem = document.createElement("li")
-  const newItem = document.createTextNode(arrayOfTodos[0].title)
-  todoListItem.appendChild(newItem)
-  todoList.appendChild(todoListItem)
-  todoListItem.classList.add("incomplete")
-}
-
 const createElement = () => {
-  const todoList = document.querySelector("#todo-list")
-  const todoListItem = document.createElement("li")
-  const newItem = document.createTextNode(arrayOfTodos.title)
-  todoListItem.appendChild(newItem)
-  todoList.appendChild(todoListItem)
-  todoListItem.classList.add("complete")
+  for (i = 0; i < 201; i++) {
+    const todoList = document.querySelector("#todo-list")
+    const todoListItem = document.createElement("li")
+    const newItem = document.createTextNode(arrayOfTodos[i].title)
+    todoListItem.appendChild(newItem)
+    todoList.appendChild(todoListItem)
+    if (arrayOfTodos[i].completed) {
+      console.log("complete")
+      todoListItem.classList.add("complete")
+    } else {
+      console.log("incomplete")
+      todoListItem.classList.add("incomplete")
+    }
+  }
 }
